a5267e324a4c3907d1ee9347f4532cad1c5694ec
yurayarosh <yura12323@gmail.com>
2020-02-14 19:53:31 +0200

    small edits `export.js`

--- scripts/export.js
+++ scripts/export.js
@@ -40,46 +40,28 @@ const convert = async () => {
   console.log('Exporting ...')
   try {
     const fullDirectoryPath = path.join(__dirname, '../pdf/')
-    const directories = getResumesFromDirectories()
-    directories.forEach(async dir => {
-      const browser = await puppeteer.launch({
-        args: ['--no-sandbox'],
-      })
-      const page = await browser.newPage()
-      await page.goto('http://localhost:4000', {
-        waitUntil: 'networkidle2',
-      })
+    const fileName = 'Yurii_Yaroshenko_CV'
 
-      if (!fs.existsSync(fullDirectoryPath)) {
-        fs.mkdirSync(fullDirectoryPath)
-      }
-      await page.pdf({
-        path: fullDirectoryPath + dir.name + '.pdf',
-        format: 'A4',
-      })
-      await browser.close()
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
     })
+    const page = await browser.newPage()
+    await page.goto('http://localhost:4000', {
+      waitUntil: 'networkidle2',
+    })
+
+    if (!fs.existsSync(fullDirectoryPath)) {
+      fs.mkdirSync(fullDirectoryPath)
+    }
+    await page.pdf({
+      path: fullDirectoryPath + fileName + '.pdf',
+      format: 'A4',
+    })
+    await browser.close()
   } catch (err) {
     throw new Error(err)
   }
   console.log('Finished exports.')
-}
-
-const getResumesFromDirectories = () => {
-  const directories = getDirectories()
-  return directories.map(dir => {
-    const filePath = dir.replace('.html', '')
-    const fileName = 'Yurii_Yaroshenko_CV'
-    return {
-      path: filePath,
-      name: fileName,
-    }
-  })
-}
-
-const getDirectories = () => {
-  const srcpath = path.join(__dirname, '../_site/')
-  return fs.readdirSync(srcpath).filter(file => file === 'index.html')
 }
 
 convert()
